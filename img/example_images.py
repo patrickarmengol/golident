cd27a45efe716a5e1ba1cb8cb83181a0d387c005
Patrick Armengol <patrickarmengol@protonmail.com>
2023-04-17 00:41:49 +0000

    example images and script to generate them

---
+++ img/example_images.py
@@ -0,0 +1,13 @@
+from golident import Golident
+
+seed = "asdfqwer"
+
+example_32 = Golident(seed, size=32, iterations=80, num_colors=5)
+example_32.save_identicon(f"img/{seed}_32.png", 8)
+example_32.show_identicon(8)
+example_32.save_history(f"img/{seed}_32.mp4")
+
+example_128 = Golident(seed, size=128, iterations=320, num_colors=5)
+example_128.save_identicon(f"img/{seed}_128.png", 2)
+example_128.show_identicon(2)
+example_128.save_history(f"img/{seed}_128.mp4")
